97d6044d0a5f4b49cd493ded15ad9e35cb58da24
ani-ruddha <aniruddhadas919@gmail.com>
2023-01-10 15:09:52 +0530

    Second Commit

---
+++ Jenkins/src/main/java/Demo/Test3.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	
+	@Test
+	public void m5()
+	{
+		System.out.println("Hello ji");
+	}
+
+}
